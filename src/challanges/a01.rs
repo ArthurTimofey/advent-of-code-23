71eb5d2a9b9d4c7a77845888001efb8c1dae8adc
Arturs.T <arturs@ArtursTs-MacBook-Pro.local>
2024-01-05 16:46:14 +0100

    checkpoint

--- src/challanges/a01.rs
+++ src/challanges/a01.rs
@@ -1,3 +1,44 @@
+fn match_first_number_in_string(str: String) -> String {
+    let regex = regex::Regex::new(r"(one|two|three|four|five|six|seven|eight|nine)").unwrap();
+
+    match regex.find(&str) {
+        Some(m) => match m.as_str() {
+            "one" => "1".to_string(),
+            "two" => "2".to_string(),
+            "three" => "3".to_string(),
+            "four" => "4".to_string(),
+            "five" => "5".to_string(),
+            "six" => "6".to_string(),
+            "seven" => "7".to_string(),
+            "eight" => "8".to_string(),
+            "nine" => "9".to_string(),
+            _ => m.as_str().to_string(),
+        },
+        None => "".to_string(),
+    }
+}
+
+fn get_first_last_number(row: &str) -> i32 {
+    if row.len() == 0 {
+        return 0;
+    }
+
+    let rev_row = row.chars().rev().collect::<String>();
+
+    println!("Row: {}, RevRow: {}", row, rev_row);
+
+    let first = match_first_number_in_string(row.to_string());
+    let last = match_first_number_in_string(row.chars().rev().collect());
+
+    println!("First: {}, Last: {}", first, last);
+
+    format!("{}{}", first, last).parse::<i32>().unwrap()
+}
+
 pub fn run(data: String) {
-    println!("Hello, world!");
+    let rows = data.split("\n");
+
+    let numbers: Vec<i32> = rows.map(|row| get_first_last_number(row)).collect();
+
+    println!("Sum: {:?}", numbers.iter().sum::<i32>());
 }
